9f60a11dc88d2915bfb9034c702f4e345e8a8fab
rjarupula <111483176+rjarupula@users.noreply.github.com>
2022-11-04 23:15:10 +0530

    Added Function to the Code

--- reversestring.py
+++ reversestring.py
@@ -1,5 +1,29 @@
+#Basic method 1
 s = input()
 r = ''
 for i in range(1, len(s)+1):
 	r += s[-i]
 print(r)
+
+#Method 2 of Function based
+def reverse(string):
+	string = string[::-1]
+	return string
+
+#Method 3 of Function based with looping process
+def reverseTwo(string):
+	str = ""
+	for i in string:
+		str = i+str
+	return str
+
+
+#Taking input from User
+string = input()
+
+#Calling functions
+#Method 2 function calling
+print(reverse(string))
+
+#Method 3 function calling
+print(reverseTwo(string))
